5a47425cf80c52607a1c4671da9916a8c744492a
Lucas Mendes dos Santos Mota <lucas.mendes0515@gmail.com>
2023-02-14 18:07:59 -0300

    Inicio do bot discord

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+//dotenv
+const dotenv = require('dotenv');
+dotenv.config();
+const{ TOKEN } = process.env
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+	console.log(`Pronto! Login realizado como ${c.user.tag}`);
+});
+
+client.login(TOKEN);
